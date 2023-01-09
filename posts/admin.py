44ee16e02125ee98c1b592f751ecd24f7a388e29
MaximK <maxim.k.elasnce@gmail.com>
2023-01-09 12:35:46 +0300

    Admin Posts registration

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from posts.models import Post, Cathegory
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Post, AuthorAdmin)
+admin.site.register(Cathegory, AuthorAdmin)
