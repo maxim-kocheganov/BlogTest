bec59ecb04dbe81dc4a3f8768867faf8491d0230
MaximK <maxim.k.elasnce@gmail.com>
2023-01-09 11:53:17 +0300

    models added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Cathegory(models.Model):
+    name = models.CharField(max_length=50)
+
+class Person(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=250)
+    created = models.DateTimeField()
+    changed = models.DateTimeField()
+    cathegory = models.ForeignKey(
+        'Cathegory',
+        on_delete=models.CASCADE,
+    )
+    is_published =  models.BooleanField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
